48ebc361fce9dc5895f95ff306f0026ba1e3ea81
PJ Fanning <pjfanning@users.noreply.github.com>
2023-06-19 17:27:19 +0100

    Update FileUtils.scala

--- src/main/scala/com/github/pjfanning/sourcedist/FileUtils.scala
+++ src/main/scala/com/github/pjfanning/sourcedist/FileUtils.scala
@@ -5,7 +5,7 @@ import java.io.File
 object FileUtils {
   def getFileNameWithoutSuffix(file: File): String = {
     val fileName = file.getName
-    val idx = fileName.lastIndexOf('.')
+    val idx      = fileName.lastIndexOf('.')
     if (idx == -1) {
       fileName
     } else {
